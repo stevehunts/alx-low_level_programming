67d6d3d33a4e8e9f7aa687d4e5ce3693f7bd9136
stevehunts <stevenvphiri95@gmail.com>
2023-12-30 14:26:43 -0800

    6-print_numberz.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,16 +1,27 @@
 #include <stdio.h>
-/**
- * main - Entry point
- * Return: Always 0 (success)
- */
 
-int main(void)
-{
-	int a;
+int main(void) {
+    putchar('0');
+    putchar('\n');
+    putchar('1');
+    putchar('\n');
+    putchar('2');
+    putchar('\n');
+    putchar('3');
+    putchar('\n');
+    putchar('4');
+    putchar('\n');
+    putchar('5');
+    putchar('\n');
+    putchar('6');
+    putchar('\n');
+    putchar('7');
+    putchar('\n');
+    putchar('8');
+    putchar('\n');
+    putchar('9');
+    putchar('\n');
 
-	for (a = 0; a < 10; a++)
-		printf("%d", a);
-	putchar('\n');
-	return (0);
+    return 0;
 }
 
