1d8d13a02633429d892467f9a6594d3fd6fd779b
stevehunts <stevenvphiri95@gmail.com>
2024-01-02 00:33:10 -0800

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int i, j;
+
+	i = 0;
+
+	while (i < 100)
+	{
+		j = i + 1;
+		while (j < 100)
+		{
+			putchar((i / 10) + '0');
+			putchar((i % 10) + '0');
+			putchar(' ');
+			putchar((j / 10) + '0');
+			putchar((j % 10) + '0');
+
+			if (!(i == 98 && j == 99))
+			{
+				putchar(',');
+				putchar(' ');
+			}
+			j++;
+		}
+		i++;
+	}
+	putchar('\n');
+
+	return (0);
+}
+
