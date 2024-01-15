f81fa04b4cbdc43fd6de5e7d98944bba37718dd9
stevehunts <stevenvphiri95@gmail.com>
2024-01-15 06:30:45 -0800

    1-main.c

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - causes an infinite loop
- * Return: 0
- */
+* main - causes an infinite loop
+* Return: 0
+*/
 
 int main(void)
 {
@@ -13,14 +13,12 @@ int main(void)
 
 	i = 0;
 
-	/*
-	 * Commented out the loop to avoid an infinite loop.
-	 * The putchar(i); statement inside the loop was missing an increment of 'i'.
-	 */
-	/* Comment here */
+	/*while (i < 10)*/
+	/*{*/
+	/*        putchar(i);*/
+	/*}*/
 
 	printf("Infinite loop avoided! \\o/\n");
 
 	return (0);
 }
-
