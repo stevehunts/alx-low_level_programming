5cdfbf99caa16e7a059dd4ef51b423ec3ed366dc
stevehunts <stevenvphiri95@gmail.com>
2024-01-18 16:09:49 -0800

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -6,17 +6,24 @@
  */
 void print_number(int n)
 {
+	int divisor = 1;
+
 	if (n < 0)
 	{
 		_putchar('-');
 		n = -n;
 	}
 
-	if (n / 10 != 0)
+	while (n / divisor >= 10)
 	{
-		print_number(n / 10);
+		divisor *= 10;
 	}
 
-	_putchar((n % 10) + '0');
+	while (divisor != 0)
+	{
+		_putchar((n / divisor) % 10 + '0');
+		divisor /= 10;
+	}
 }
+
 
