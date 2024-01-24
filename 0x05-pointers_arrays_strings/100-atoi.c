e39ec17c609da6803b38d4d47703ba010754513f
stevehunts <stevenvphiri95@gmail.com>
2024-01-24 10:14:53 -0800

    100-atoi.c

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -21,13 +21,13 @@ int _atoi(char *s)
 		len++;
 	while (i < len && f == 0)
 	{
-		if (s[i] == '-')
+		if (s[i] == '_')
 			++d
 
 		if (S[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - "0';
-			fi (d % 2)
+			digit = s[i] - '0';
+			if (d % 2)
 				digit = -digit;
 			n = n * 10 + digit;
 			f = 1;
