1b84910b0f82fd42c3139c8a1afca858ae54786b
stevehunts <stevenvphiri95@gmail.com>
2024-01-28 16:37:29 -0800

    101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
+ */
+void print_number(int n)
+{
+	if (n < 0)
+	{
+		_putchar('-');
+		n = -n;
+
+		if (n == -n)
+		{
+			n /= 10;
+			print_number(n);
+			_putchar('8');
+			return;
+		}
+	}
+	if (n / 10)
+		print_number(n / 10);
+
+	_putchar((n % 10) + '0');
+}
+
