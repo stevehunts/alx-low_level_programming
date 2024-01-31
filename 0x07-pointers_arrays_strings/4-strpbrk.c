e20ee2d460bd581b65e312627b12397331f1ed56
stevehunts <stevenvphiri95@gmail.com>
2024-01-31 15:43:18 -0800

    4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -10,20 +10,19 @@
  */
 char *_strpbrk(char *s, char *accept)
 {
-	char *a_ptr;
+	int k;
 
 	while (*s)
 	{
-		for (a_ptr = accept; *a_ptr; a_ptr++)
+		for (k = 0; accept[k]; k++)
 		{
-			if (*s == *a_ptr)
+			if (*s == accept[k])
 			{
 				return (s);
 			}
 		}
 		s++;
 	}
-
-	return (NULL);
+	return ('\0');
 }
 
