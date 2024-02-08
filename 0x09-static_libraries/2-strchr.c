eb4c76df0e38d8238aa5709553a07780ce469db2
stevehunts <stevenvphiri95@gmail.com>
2024-02-07 23:47:26 -0800

    libmy.a

---
+++ 0x09-static_libraries/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string to search in
+ * @c: character to locate
+ *
+ * Return: pointer to the first occurrence of c in s, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
+}
