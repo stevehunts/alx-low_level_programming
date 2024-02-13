5602aafceb1ad4e40266273176aec671a7648e78
stevehunts <stevenvphiri95@gmail.com>
2024-02-13 01:21:19 -0800

    4-add.c

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * Check num - if a string represents a positive number.
+ * @str: array str
+ *
+ * Return: Always 0 success
+ */
+int is_positive_number(char *str)
+{
+	unsigned int count;
+
+	count = 0;
+	while (count < strlen(str))
+	{
+		if (!isdigit(str[count]))
+
+		{
+			return (0);
+		}
+		count++;
+	}
+	return (1);
+}
+
+/**
+ * main - print the name of the program
+ * @argc: Count arguments
+ * @argv: Arguments
+ *
+ * Return: Always 0 success
+ */
+int main(int argc, char *argv[])
+{
+	int count;
+	int str_to_int;
+	int sum = 0;
+
+	count = 1;
+	while (count < argc)
+	{
+	if (check_num(argv[count]))
+	{
+	str_to_int = atoi(argv[count]);
+
+	sum += str_to_int;
+	}
+
+		else
+		{
+		print("Error\n");
+		return (1);
+		}
+		count++;
+	}
+	print("%d\n", sum);
+	return (0);
+}
