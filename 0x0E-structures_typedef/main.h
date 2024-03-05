5b174cb0f6a42a62e493e9387ae3961ac49c6169
stevehunts <stevenvphiri95@gmail.com>
2024-03-05 03:43:34 -0800

    1-init_dog.c

--- 0x0E-structures_typedef/main.h
+++ 0x0E-structures_typedef/main.h
@@ -1,8 +1,19 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - a dog's basic info
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ */
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-void print_dog(struct dog *d);
-dog_t *new_dog(char *name, float age, char *owner);
 
-#endif
+#endif /* DOG_H */
+
