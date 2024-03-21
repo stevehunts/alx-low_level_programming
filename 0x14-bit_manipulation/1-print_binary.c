cba8adb26686c69bf499d19c0dc2bb1653b1dd4e
stevehunts <stevenvphiri95@gmail.com>
2024-03-21 05:22:46 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,44 +1,51 @@
 #include "main.h"
 
 /**
- * _pow - func calculates (base ^ power)
+ * _pow - calculates (base ^ power)
  * @base: base of the exponent
  * @power: power of the exponent
  *
- * Return: value of (base ^ power0
+ * Return: value of (base ^ power)
  */
 unsigned long int _pow(unsigned int base, unsigned int power)
 {
-	unsigned long int num;
-	unsigned int a;
+	unsigned long int num = 1;
+	unsigned int i;
 
-	num = 1;
-	for (a = 1; a <= power; a++)
+	for (i = 0; i < power; i++)
+	{
 		num *= base;
-	return (num);
+	}
+
+	return (num); /* Corrected return statement with parentheses */
 }
 
 /**
  * print_binary - prints a number in binary notation
  * @n: number to print
- *
- * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	unsigned long int divisor, check;
-	char flag;
+	unsigned long int divisor;
+	int started = 0; /* Tracking if we've started printing non-zero bits */
 
-	flag = 0;
-	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
-	while (divisor != 0)
+	if (n == 0)
 	{
-		check = n & divisor;
-		if (check == divisor)
+		_putchar('0');
+		return; /* Corrected return statement with parentheses */
+	}
+
+	for (divisor = _pow(2, sizeof(n) * 8 - 1); divisor > 0; divisor >>= 1)
+	{
+		if (n & divisor)
 		{
-			flag = 1;
 			_putchar('1');
+			started = 1;
 		}
-		divisor >>= 1;
+		else if (started)
+		{
+			_putchar('0');
+		}
 	}
 }
+
