46e05855fe4c4af648fee355572cba5b586ca8c1
stevehunts <stevenvphiri95@gmail.com>
2024-03-21 04:53:15 -0700

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,49 +1,44 @@
-#include <stdio.h>
-#include <unistd.h> /* For write function prototype */
+#include "main.h"
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * _pow - func calculates (base ^ power)
+ * @base: base of the exponent
+ * @power: power of the exponent
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: value of (base ^ power0
  */
-int _putchar(char c)
+unsigned long int _pow(unsigned int base, unsigned int power)
 {
-	return (write(1, &c, 1)); /* Ensuring use of tabs for indentation */
+	unsigned long int num;
+	unsigned int a;
+
+	num = 1;
+	for (a = 1; a <= power; a++)
+		num *= base;
+	return (num);
 }
 
 /**
- * print_binary - Prints the binary representation of a number.
- * @n: The number to be printed in binary.
+ * print_binary - prints a number in binary notation
+ * @n: number to print
+ *
+ * Return: void
  */
 void print_binary(unsigned long int n)
 {
-	if (n > 1)
+	unsigned long int divisor, check;
+	char flag;
+
+	flag = 0;
+	divisor = _pow(2, sizeof(unsigned long int) * 8 - 1);
+	while (divisor != 0)
 	{
-		print_binary(n >> 1); /* Ensuring use of tabs for indentation */
+		check = n & divisor;
+		if (check == divisor)
+		{
+			flag = 1;
+			_putchar('1');
+		}
+		divisor >>= 1;
 	}
-
-	_putchar((n & 1) + '0'); /* Printing the least significant bit */
-}
-
-/**
- * main - Entry point
- * Description: Test the print_binary function with various numbers
- *
- * Return: Always 0 (success)
- */
-int main(void)
-{
-	print_binary(0);
-	printf("\n");
-	print_binary(1);
-	printf("\n");
-	print_binary(98);
-	printf("\n");
-	print_binary(1024);
-	printf("\n");
-	print_binary((1 << 10) + 1);
-	printf("\n");
-	return (0);
 }
