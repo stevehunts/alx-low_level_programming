4809f7ac7ea1a80a0fb73a8d395ccb5c1d20ea73
stevehunts <stevenvphiri95@gmail.com>
2024-03-25 08:14:42 -0700

    5-free_listint2.c

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -5,7 +5,8 @@
  * @head: A pointer to the address of the head of listint_t list
  *
  * Description: Sets the head to NULL.
- */void free_listint2(listint_t **head)
+ */
+void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
