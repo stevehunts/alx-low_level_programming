b5427a30bd41cf27f7e577ec7190d17ef34a3f27
stevehunts <stevenvphiri95@gmail.com>
2024-03-25 05:36:41 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,9 +1,8 @@
 #include "lists.h"
-
 /**
  * print_listint - prints all the elements of a listint_t list
+ * 
  * @h: pointer to the start of the list
- *
  * Return: the number of nodes in the list
  */
 size_t print_listint(const listint_t *h)
