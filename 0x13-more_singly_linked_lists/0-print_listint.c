fe4c6d3734e89fdc144951f4255158ffc13dfa58
stevehunts <stevenvphiri95@gmail.com>
2024-03-26 03:47:41 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,13 +11,12 @@ size_t print_listint(const listint_t *h)
 {
 	size_t nodes = 0;
 
-	while (h != NULL)
+	while (h)
 	{
+		nodes++;
 		printf("%d\n", h->n);
 		h = h->next;
-		nodes++;
 	}
 
 	return (nodes);
 }
-
